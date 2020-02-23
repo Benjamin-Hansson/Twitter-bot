2aca9f1a0ddf160d1bd7ee0a831ad3ac7f49411d
Benjamin Hansson <benha109@student.liu.se>
2020-02-23 21:29:05 +0100

    first commit

--- twitt.py
+++ twitt.py
@@ -1,47 +1,53 @@
 import tweepy
 from googletrans import Translator, LANGCODES, LANGUAGES
 import random
+from config import consumer_key, consumer_secret, access_token, access_token_secret
+
 from time import sleep
 import json
 
-consumer_key = "YzNdOZf6LJM9RWRxh8JMMCOd7"
-consumer_secret = "xgNduSLjdZME7pAzC8VEM52us7S5qRC8jkeELvrElzZD4445RT"
-access_token = "1090333056844660738-8J6nDosbKVTlPs4D5QXMdu4KPJVup2"
-access_token_secret = "YHFIU3sTJoaVBmdxzBei9ywHdD6CVUSaE5gDGkaedMH5v"
-
-#login
-
+# login
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 last_id = ""
-while(True):
 
-    a = api.user_timeline(screen_name="realdonaldtrump", tweet_mode='extended')
-    if(len(a) != 0):
-        tweet = a[0].full_text
-        if(last_id != a[0].id_str):
-            url = ""
-            print("--------------------------")
-            print("original: " + tweet)
-            #find if there is an url in the tweet
-            if(tweet.find("http") != -1):
-                index = tweet.index("http")
-                url = tweet[index:]
-                tweet = tweet[:index]
-            #translation
-            translator = Translator()
-            lang1, lang2 = "en", ""
-            for i in range(30):
-                lang2 = random.choice(list(LANGUAGES.items()))[0]
-                tweet = translator.translate(text=tweet, src=lang1, dest=lang2).text
-                lang1 = lang2
-            tweet = translator.translate(text=tweet, src=lang1, dest= 'en').text
-            #if there is an url in the tweet
-            if(url != ""):
-                tweet = tweet + " " + url
-            print("new: " + tweet)
-            api.update_status(status="@realdonaldtrump " + tweet, in_reply_to_status_id=a[0].id_str)
-            print("--------------------------")
-            last_id = a[0].id_str
-    sleep(10)
+while (True):
+	a = api.user_timeline(screen_name="realdonaldtrump", tweet_mode='extended')
+
+	if a:
+		for i in range(len(a)):
+
+			translator = Translator()
+
+			tweet = a[i].full_text
+
+			print("Tweet:", tweet)
+
+			# & seems to crash the translator
+			tweet = tweet.replace("&", "and ")
+
+			# Preserve the url if there is one
+			index = tweet.find("http")
+			url = ""
+			if index != -1:
+				url = tweet[index:]
+				tweet = tweet[:index]
+				print(index)
+
+			# Translate 30 times (100 takes to long time and there is a limit on number of requests to the api)
+			# Idea still the same though
+			lang1, lang2 = "en", ""
+			"""
+			for i in range(30):
+				lang2 = random.choice(list(LANGUAGES.items()))[0] # choose a random language
+				tweet = translator.translate(text=tweet, src=lang1, dest=lang2).text
+				lang1 = lang2
+			tweet = translator.translate(text=tweet, src=lang1, dest='en').text # translate back to english
+			tweet = tweet + " " + url # add back the url
+			print("new: " + tweet)
+
+			"""
+	# api.update_status(status="@realdonaldtrump " + tweet, in_reply_to_status_id=a[0].id_str)
+	# print("--------------------------")
+
